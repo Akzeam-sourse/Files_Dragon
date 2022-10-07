9cfa9c8ea2b58285bad7947ff89c3e13dbc15430
Akzeam-sourse <113262496+Akzeam-sourse@users.noreply.github.com>
2022-10-07 18:05:18 +0300

    Update ChangeName.lua

--- Files_Dragon/ChangeName.lua
+++ Files_Dragon/ChangeName.lua
@@ -4,10 +4,10 @@ if ChatType == 'sp' or ChatType == 'gp'  then
 if text then 
 tdcli_function({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,result)
 if result.id_ then 
-local SOFI = DevSOFI:get("DraGoN:Name"..result.id_)
+local SOFI = DevSOFI:get("XFor:Name"..result.id_)
 if not result.first_name_ then 
 if SOFI then 
-DevSOFI:del("DraGoN:Name"..result.id_) 
+DevSOFI:del("XFor:Name"..result.id_) 
 end
 end
 if result.first_name_ then 
@@ -21,7 +21,7 @@ local SOFI_text = {
 SOFIs = math.random(#SOFI_text)
 Dev_SOFI(msg.chat_id_, msg.id_, 1, SOFI_text[SOFIs], 1, 'html')
 end  
-DevSOFI:set("DraGoN:Name"..result.id_, result.first_name_)  
+DevSOFI:set("XFor:Name"..result.id_, result.first_name_)  
 end
 end
 end,nil) 
@@ -30,5 +30,5 @@ end
 
 end
 return {
-Dragon = ChangeName
+XFor = ChangeName
 }
