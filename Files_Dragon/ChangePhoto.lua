d249a244e811e66c299d95d0d4026df51687ac0f
Akzeam-sourse <113262496+Akzeam-sourse@users.noreply.github.com>
2022-10-07 18:06:03 +0300

    Update ChangePhoto.lua

--- Files_Dragon/ChangePhoto.lua
+++ Files_Dragon/ChangePhoto.lua
@@ -4,11 +4,11 @@ if ChatType == 'sp' or ChatType == 'gp'  then
 if text then 
 tdcli_function({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,result)
 if result.id_ then 
-local SOFI = DevSOFI:get("DraGoN:Photo"..result.id_)
+local SOFI = DevSOFI:get("XFor:Photo"..result.id_)
 if not result.profile_photo_ then 
 if SOFI then 
 Dev_SOFI(msg.chat_id_, msg.id_, 1, "حذف كل صوره مضروب بوري، 😹💔", 1, 'html')
-DevSOFI:del("DraGoN:Photo"..result.id_) 
+DevSOFI:del("XFor:Photo"..result.id_) 
 end
 end
 if result.profile_photo_ then 
@@ -22,7 +22,7 @@ local SOFI_text = {
 SOFI3 = math.random(#SOFI_text)
 Dev_SOFI(msg.chat_id_, msg.id_, 1, SOFI_text[SOFI3], 1, 'html')
 end  
-DevSOFI:set("DraGoN:Photo"..result.id_, result.profile_photo_.big_.persistent_id_) 
+DevSOFI:set("XFor:Photo"..result.id_, result.profile_photo_.big_.persistent_id_) 
 end
 end
 end,nil) 
@@ -31,5 +31,5 @@ end
 
 end
 return {
-Dragon = ChangePhoto
+XFor = ChangePhoto
 }
