fa5f14bc9d7eb5dff8e9fc2cb24d85c7541fe43e
Akzeam-sourse <113262496+Akzeam-sourse@users.noreply.github.com>
2022-10-07 18:04:15 +0300

    Update AddedMe.lua

--- Files_Dragon/AddedMe.lua
+++ Files_Dragon/AddedMe.lua
@@ -2,13 +2,13 @@ local function AddedMe(msg)
 local text = msg.content_.text_
 if ChatType == 'sp' or ChatType == 'gp'  then
 if text and text:match("منو ضافني") then
-if not DevSOFI:get(Dragon..'SOFI:Added:Me'..msg.chat_id_) then
+if not DevSOFI:get(XFor..'SOFI:Added:Me'..msg.chat_id_) then
 tdcli_function ({ID = "GetChatMember",chat_id_ = msg.chat_id_,user_id_ = msg.sender_user_id_},function(arg,da) 
 if da and da.status_.ID == "ChatMemberStatusCreator" then
 Dev_SOFI(msg.chat_id_, msg.id_, 1, '⌯︙انت منشئ المجموعه', 1, 'md') 
 return false
 end
-local Added_Me = DevSOFI:get(Dragon.."Who:Added:Me"..msg.chat_id_..':'..msg.sender_user_id_)
+local Added_Me = DevSOFI:get(XFor.."Who:Added:Me"..msg.chat_id_..':'..msg.sender_user_id_)
 if Added_Me then 
 tdcli_function ({ID = "GetUser",user_id_ = Added_Me},function(extra,result,success)
 local Name = '['..result.first_name_..'](tg://user?id='..result.id_..')'
@@ -25,16 +25,16 @@ end
 end
 
 if text == 'تفعيل ضافني' and Manager(msg) then 
-DevSOFI:del(Dragon..'SOFI:Added:Me'..msg.chat_id_) 
+DevSOFI:del(XFor..'SOFI:Added:Me'..msg.chat_id_) 
 Dev_SOFI(msg.chat_id_, msg.id_, 1, '⌯︙تم تفعيل امر منو ضافني', 1, 'md') 
 end
 if text == 'تعطيل ضافني' and Manager(msg) then 
-DevSOFI:set(Dragon..'SOFI:Added:Me'..msg.chat_id_,true) 
+DevSOFI:set(XFor..'SOFI:Added:Me'..msg.chat_id_,true) 
 Dev_SOFI(msg.chat_id_, msg.id_, 1, '⌯︙تم تعطيل امر منو ضافني', 1, 'md') 
 end
 end
 
 end
 return {
-Dragon = AddedMe
+XFor = AddedMe
 }
